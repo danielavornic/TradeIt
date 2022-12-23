6a653fe1938b08e0813caf3ac065f86eb1409c30
Daniela Vornic <daniela.vornic@gmail.com>
2022-12-23 23:10:53 +0200

    Remove home redirect in user context

--- context/UserContext.tsx
+++ context/UserContext.tsx
@@ -9,7 +9,9 @@ import {
   signInWithPopup,
 } from "firebase/auth";
 
+import { useMutation } from "react-query";
 import app from "../firebase";
+import { user as userApi } from "../api";
 import { Loader } from "../components";
 import { useRouter } from "next/router";
 
@@ -46,6 +48,15 @@ export const UserProvider = ({ children }: { children: React.ReactNode }) => {
   const [idToken, setIdToken] = useState<string | undefined>(undefined);
   const [isLoading, setIsLoading] = useState(true);
 
+  const { mutate: addUser } = useMutation(userApi.add, {
+    onSuccess: () => {
+      console.log("User added to database");
+    },
+    onError: (error) => {
+      console.log(error);
+    },
+  });
+
   const errorToast = (message: string) =>
     toast({
       title: "Error",
@@ -61,9 +72,6 @@ export const UserProvider = ({ children }: { children: React.ReactNode }) => {
       .then((userCredential) => {
         const user = userCredential.user;
         setUser(user);
-        console.log(user);
-
-        setLogged(true);
       })
       .catch((error) => {
         const errorMessage = error.message;
@@ -119,7 +127,6 @@ export const UserProvider = ({ children }: { children: React.ReactNode }) => {
         setUser(user);
         setLogged(true);
         setIsLoading(false);
-        router.push("/home");
       } else {
         setUser(null);
         setLogged(false);
