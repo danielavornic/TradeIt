7dae22e7cc5822f810ea270942b4ac24fba0de1e
Daniela Vornic <daniela.vornic@gmail.com>
2022-12-23 17:10:27 +0200

    Updae firebase config

--- firebase/index.ts
+++ firebase/index.ts
@@ -2,12 +2,12 @@ import { initializeApp } from "firebase/app";
 import { getStorage } from "firebase/storage";
 
 const firebaseConfig = {
-  apiKey: "AIzaSyDPIBSH-hP_dL3b2smuxYWqVUffkJusJf0",
-  authDomain: "tradeit-479fb.firebaseapp.com",
-  projectId: "tradeit-479fb",
-  storageBucket: "tradeit-479fb.appspot.com",
-  messagingSenderId: "50864115695",
-  appId: "1:50864115695:web:9bb7a022019ed62b96301a",
+  apiKey: "AIzaSyBYMYV1FAhRNENAGsn1GpxgZIZ_6eCvftI",
+  authDomain: "trade-a95eb.firebaseapp.com",
+  projectId: "trade-a95eb",
+  storageBucket: "trade-a95eb.appspot.com",
+  messagingSenderId: "655202702412",
+  appId: "1:655202702412:web:eb9ed5d740f096f8cc50e9",
 };
 
 const app = initializeApp(firebaseConfig);
