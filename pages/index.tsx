bfa97754827feb5c928e9f690a5fe6a1ef486cf4
Daniela Vornic <daniela.vornic@gmail.com>
2022-12-23 23:08:07 +0200

    Add request to fetch all products in homepage

--- pages/index.tsx
+++ pages/index.tsx
@@ -1,5 +1,10 @@
-import { Layout, SignupForm } from "../components";
+import { useQuery } from "react-query";
+
+import { Layout } from "../components";
+import { products } from "../api";
 
 export default function Home() {
+  const { data } = useQuery("products", products.getList);
+
   return <Layout title='TradeIt - Login'>hello</Layout>;
 }
