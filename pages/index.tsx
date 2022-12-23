795d6632eb412a675478c43c1400a91c6cd024fc
Daniela Vornic <daniela.vornic@gmail.com>
2022-12-23 21:57:22 +0200

    Update index page

--- pages/index.tsx
+++ pages/index.tsx
@@ -1,24 +1,5 @@
-import { Flex, Heading, Stack } from "@chakra-ui/react";
 import { Layout, SignupForm } from "../components";
 
 export default function Home() {
-  return (
-    <Layout title='TradeIt - Login' hasNavbar={false}>
-      <Flex
-        minH={"100vh"}
-        minW='32'
-        align={"center"}
-        justify={"center"}
-        bg='gray.50'
-      >
-        <Stack spacing={8} mx={"auto"} maxW={"xl"} mt='-100px' px={4}>
-          {/* <Image src='logo512.png' width={100} height={100} alt='logo' /> */}
-          <Heading fontSize={"4xl"} textAlign='center'>
-            Sign up to TradeIt
-          </Heading>
-          <SignupForm />
-        </Stack>
-      </Flex>
-    </Layout>
-  );
+  return <Layout title='TradeIt - Login'>hello</Layout>;
 }
