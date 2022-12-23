a54df4c7c2a30a41c78786af23e783beab0faf49
Alex <alexander.prohnitchi@gmail.com>
2022-12-23 21:38:41 +0200

    small fix

--- pages/product.tsx
+++ pages/product.tsx
@@ -1,18 +1,10 @@
 import { Flex, Heading, Stack } from "@chakra-ui/react";
-import { Layout, LogInForm } from "../components";
+import { Layout, LogInForm, Navbar } from "../components";
 
-const Login = () => {
+const Product = () => {
   return (
     <Layout title='TradeIt - Login'>
-      <Flex minH={"100vh"} align={"center"} justify={"center"} bg='gray.50'>
-        <Stack spacing={8} mx={"auto"} maxW={"xl"} mt='-100px' px={4}>
-          {/* <Image src='logo512.png' width={100} height={100} alt='logo' /> */}
-          <Heading fontSize={"4xl"} textAlign='center'>
-            Sign in to TradeIt
-          </Heading>
-          <LogInForm />
-        </Stack>
-      </Flex>
+      <Navbar />
     </Layout>
   );
 };
